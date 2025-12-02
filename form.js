f5dad07f3a4ddc657290d45fba502f587a90fbad
Miles <miles.christiansen@southeasttech.edu>
2025-12-02 09:55:25 -0600

    Fix

--- form.js
+++ form.js
@@ -2,7 +2,8 @@ document.getElementById('SubBtn').addEventListener('click', function(event) {
     alert("Form Submitted Successfully!");
     event.preventDefault();
     const ageValue = parseInt(document.getElementById('Age').value, 10);
+    const ageValueChange = parseInt(document.getElementById('Age').value+40, 10);
     if (!isNaN(ageValue) && ageValue >= 0 && ageValue <= 255) {
-        document.body.style.backgroundColor = `rgb(${ageValue}, ${ageValue}, ${ageValue})`;
+        document.body.style.backgroundColor = `rgb(${ageValue}, ${ageValueChange}, ${ageValue})`;
     }
 });
