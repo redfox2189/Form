676ab7894d57106c8e658115c8c741d7192fd09f
Miles <miles.christiansen@southeasttech.edu>
2025-12-02 09:45:30 -0600

    Fix

--- form.js
+++ form.js
@@ -4,7 +4,5 @@ document.getElementById('SubBtn').addEventListener('click', function(event) {
     const ageValue = parseInt(document.getElementById('Age').value, 10);
     if (!isNaN(ageValue) && ageValue >= 0 && ageValue <= 255) {
         document.body.style.backgroundColor = `rgb(${ageValue}, ${ageValue}, ${ageValue})`;
-    } else {
-        alert("Please enter a valid age between 5 and 120.");
     }
 });
